7fa35e8eb4e68be28278f05658726c1d66665104
Marvin <lechuxacaderon@gmail.com>
2026-03-18 20:28:46 -0600

    Clase Biblioteca con listas ya fue creada

---
+++ BibliotecaApp/Datos/Biblioteca.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotecaApp.Clases;
+
+namespace BibliotecaApp.Datos
+{
+    public class Biblioteca
+    {
+        public List<Libro> Libros { get; set; } = new List<Libro>();
+        public List<Usuario> Usuarios { get; set; } = new List<Usuario>();
+        public List<Prestamo> Prestamos { get; set; } = new List<Prestamo>();
+
+        // Metodos Libros
+
+        public void AgregarLibro(Libro libro) => Libros.Add(libro);
+        public void EliminarLibro(Libro libro) => Libros.Remove(libro);
+
+        // Métodos Usuarios
+        public void AgregarUsuario(Usuario usuario) => Usuarios.Add(usuario);
+        public void EliminarUsuario(Usuario usuario) => Usuarios.Remove(usuario);
+
+        // Métodos Préstamos
+        public void RegistrarPrestamo(Prestamo prestamo) => Prestamos.Add(prestamo);
+
+
+    }
+}
